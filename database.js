dc497b9023b3240ab251b5514c2fdc8600df395e
Kiril Nikolov <kirqkov2309@gmail.com>
2025-02-20 18:08:13 +0200

    first steps to authentication

--- database.js
+++ database.js
@@ -1,4 +1,5 @@
 const sqlite3 = require("sqlite3").verbose();
+const bcrypt = require("bcryptjs");
 
 // Open (or create) the SQLite database
 const db = new sqlite3.Database("./jobs.db", (err) => {
@@ -9,7 +10,6 @@ const db = new sqlite3.Database("./jobs.db", (err) => {
   }
 });
 
-// Create the Jobs table if it doesn't exist
 db.run(
   `CREATE TABLE IF NOT EXISTS jobs (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -17,5 +17,31 @@ db.run(
     color TEXT NOT NULL
   )`
 );
+
+db.run(`
+  CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    email TEXT UNIQUE NOT NULL,
+    password TEXT NOT NULL,
+    google_sheets_id TEXT
+  )
+`);
+
+const registerUser = async (email, password, callback) => {
+  const hashedPassword = await bcrypt.hash(password, 10);
+  db.run(
+    "INSERT INTO users (email, password) VALUES (?, ?)",
+    [email, hashedPassword],
+    function (err) {
+      callback(err, { id: this?.lastID, email });
+    }
+  );
+};
+
+const findUserByEmail = (email, callback) => {
+  db.get("SELECT * FROM users WHERE email = ?", [email], (err, user) => {
+    callback(err, user);
+  });
+};
 
 module.exports = db;
